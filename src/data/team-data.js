dca91f3055a7235048c8765458f0b3cb60612ee8
ben <bsaenz454@gmail.com>
2024-09-07 13:03:08 -0400

    git init

---
+++ src/data/team-data.js
@@ -0,0 +1,156 @@
+export const teamData = [
+  {
+    img: "",
+    name: "Lance Moreau",
+    position: "Mentor Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Ayush Mishra",
+    position: "Web Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  
+  {
+    // img: "/img/team-3.jpg",
+    name: "Benjamin Saenz",
+    position: "Web Liason",
+    socials: [
+      // {
+      //   color: "light-blue",
+      //   name: "twitter",
+      // },
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+      // {
+      //   color: "pink",
+      //   name: "dribbble",
+      // },
+    ],
+  },
+  {
+    img: "",
+    name: "Brayson Fleegle",
+    position: "Finance Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "David Fritz",
+    position: "Team Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Georgia",
+    position: "Web Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Jonathan Jayaputra",
+    position: "TBD",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Joseph Bailer",
+    position: "Finance Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Kevin Li",
+    position: "TBD",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Md Rahman",
+    position: "Role",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Nicolas Preda",
+    position: "TBD",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Sasha Brown",
+    position: "TBD",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+  {
+    img: "",
+    name: "Thomas Domorad",
+    position: "Library Liason",
+    socials: [
+      {
+        color: "blue",
+        name: "linkedin",
+      },
+    ],
+  },
+];
+
+export default teamData;
